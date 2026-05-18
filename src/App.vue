42b1a9a0792c6fe362f2bf510167194ec13feec5
a-klymenko <a.klymenko@student.itc.khai.edu>
2026-05-18 23:15:19 +0300

    Restore top navigation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const authPages = ['/register', '/login']
+
+function isAuthPage() {
+  return authPages.includes(route.path)
+}
 </script>
 
 <template>
-  <RouterView />
+  <div>
+    <header v-if="!isAuthPage()" class="app-header">
+      <RouterLink to="/levels" class="brand">Інфографіка</RouterLink>
+
+      <nav class="main-nav">
+        <RouterLink to="/levels">Рівні</RouterLink>
+        <RouterLink to="/library">Бібліотека</RouterLink>
+        <RouterLink to="/profile">Персонаж</RouterLink>
+        <RouterLink to="/project">Проєкт</RouterLink>
+        <RouterLink to="/gallery">Галерея</RouterLink>
+        <RouterLink to="/certificate">Сертифікат</RouterLink>
+      </nav>
+    </header>
+
+    <main :class="['app-main', { 'with-header': !isAuthPage() }]">
+      <RouterView :key="route.fullPath" />
+    </main>
+  </div>
 </template>
